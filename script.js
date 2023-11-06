2e760e31a929d56c8815e04338d506fb89fec892
AleAngheben <144798020+AleAngheben@users.noreply.github.com>
2023-11-06 16:17:28 +0100

    miss only compare owner

---
+++ script.js
@@ -0,0 +1,109 @@
+// ESERCIZIO 1
+
+// creo costruttore
+class User {
+  constructor(firstName, lastName, age, location) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.location = location;
+  }
+  static compareAge(firstUser, secondUser) {
+    if (firstUser.age > secondUser.age) {
+      console.log(
+        firstUser.firstName + " è più grande di " + secondUser.firstName
+      );
+    } else if (firstUser.age < secondUser.age) {
+      console.log(
+        firstUser.firstName + " è più giovane di " + secondUser.firstName
+      );
+    } else {
+      console.log(
+        firstUser.firstName +
+          " e " +
+          secondUser.firstName +
+          " hanno la stessa età"
+      );
+    }
+  }
+}
+
+// creo oggetti
+const utente1 = new User("Mario", "Palladino", 24, "Torino");
+console.log(utente1);
+
+const utente2 = new User("Lara", "Medaglia", 27, "Milano");
+console.log(utente2);
+
+const utente3 = new User("Alessandro", "Angheben", 24, "Trento");
+console.log(utente3);
+
+// COMPARATO ETA' TRAMITE METODO
+User.compareAge(utente1, utente2);
+User.compareAge(utente1, utente3);
+User.compareAge(utente2, utente3);
+
+// ESERCIZIO 2
+class Pet {
+  constructor(petName, ownerName, species, breed) {
+    this.petName = petName;
+    this.ownerName = ownerName;
+    this.species = species;
+    this.breed = breed;
+  }
+  static sameOwner(pet1, pet2) {
+    if (pet1.ownerName === pet2.ownerName) {
+      console.log(true);
+    } else {
+      console.log(false);
+    }
+  }
+}
+
+const petForm = document.getElementById("pet-form");
+const petList = document.getElementById("pet-list");
+
+const pets = [];
+
+petForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const petName = document.getElementById("pet-name").value;
+
+  const ownerName = document.getElementById("owner-name").value;
+  const species = document.getElementById("species").value;
+  const breed = document.getElementById("breed").value;
+
+  const pet = new Pet(petName, ownerName, species, breed);
+  pets.push(pet);
+
+  myList();
+
+  petForm.reset();
+});
+
+function myList() {
+  petList.innerHTML = " ";
+  pets.forEach(function (pet) {
+    const myLi = document.createElement("li");
+    myLi.textContent =
+      "Pet name = " +
+      pet.petName +
+      " , " +
+      "Owner name = " +
+      pet.ownerName +
+      " , " +
+      "Species = " +
+      pet.species +
+      " , " +
+      "Breed = " +
+      pet.breed;
+    petList.appendChild(myLi);
+  });
+}
+
+for (let i = 0; i < pets.length; i++) {
+  for (let j = i + 1; j < pets.length; j++) {
+    Pet.sameOwner(i, j);
+  }
+}
